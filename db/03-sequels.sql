5ccbd0d48f8672adfaf607bf1a663801ad659363
aswain03 <alexander.swain03@gmail.com>
2023-08-01 15:43:44 +0100

    refactors repo to compliment Ncore

--- db/03-sequels.sql
+++
@@ -1,4 +0,0 @@
-\c nc_flix
-
-
-\echo '\n 1. The store with the highest total of sequels is:'
--- db/04-customers.sql
+++
@@ -1,7 +0,0 @@
-\c nc_flix
-
-
-\echo '\n 1. Here is the information about the customers and their loyalty status:'
-
-
-\echo '\n 2. Here is a comprehensive output of the customers ordered by loyalty points, and grouped by area:'
